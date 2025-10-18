a1b423bc1e8ce9302b93fd74052e2f09b2cafefa
Kudos Ajao <ajaokudos34@gmail.com>
2025-10-18 13:45:03 +0100

    initial commit

---
+++ database/db.py
@@ -0,0 +1,10 @@
+import sqlite3
+class User:
+    def __init__(self,conn):
+        self.cur = conn.cursor()
+    
+    def create_table(self):
+        self.cur.execute("""
+""")
+           
+            
