ce7d6e4fc8e6ffd0695583187da2c685f3bbac2a
Kudos Ajao <ajaokudos34@gmail.com>
2025-10-20 14:33:39 +0100

    solved_decorator(untested)

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,8 +2,6 @@ import time
 import os
 from pathlib import Path
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
@@ -15,11 +13,10 @@ from utils import *
 BASE_DIR:str = Path(__file__).resolve().parent
 UPLOADS_DIR:str = os.path.join(BASE_DIR,"uploads")
 FILE_LOCATION:str = lambda file:os.path.join(UPLOADS_DIR,file)
-
+DRIVER = configure_driver()
 
 class ReceiptDownloader:
     def __init__(self,download_loc:str):
-        self.chrome_options = Options()
         prefs = {
             "download.default_directory":FILE_LOCATION(download_loc),
             "profile.default_content_settings.popups":0,
@@ -27,14 +24,12 @@ class ReceiptDownloader:
             "profile.default_content_setting_values.automatic_downloads":1,
             "safebrowsing.enabled":False
             }
-        add_options(self.chrome_options,prefs)
-        self.driver = webdriver.Chrome(options=self.chrome_options,service=Service(ChromeDriverManager().install()))
+        self.driver = configure_driver(prefs)
         url = "https://ecampus.fuotuoke.edu.ng/ecampus/login.html"
         self.driver.get(url)
         self.driver.implicitly_wait(30)
 
-
-    @check_for_toggle(self.driver)
+    @check_for_toggle(DRIVER)
     def login(self,name:str,password:str):
         username_box = self.driver.find_element(By.ID,"username")
         username_box.clear()
@@ -46,7 +41,6 @@ class ReceiptDownloader:
         password_box.send_keys(password)
         signin_btn = self.driver.find_element(By.ID,"btn_login")
         signin_btn.click()
-
 
 class CoursesDownloader:
     pass
