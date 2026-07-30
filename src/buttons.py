5c15ca4b53af3bb23326233e76605e566bf3eead
Cipher <ajaokudos34@gmail.com>
2026-07-30 17:33:04 +0100

    fixed a minor bug

--- src/buttons.py
+++ src/buttons.py
@@ -4,12 +4,12 @@ from aiogram.types import InlineKeyboardButton
 
 def get_service_buttons():
     builder = InlineKeyboardBuilder()
-    builder.button("Download Receipts 🧾", callback_data="download_receipts")
-    builder.button("Download Results 📄", callback_data="download_results")
-    builder.button("Get Result Summary 🧾", callback_data="result_summary")
-    builder.button("Download Courses 📄", callback_data="download_courses")
-    builder.button("Download_Biodata 🧾", callback_data="download_biodata")
-    builder.button("Download Admission Forms 📄", callback_data="admission_forms")
+    builder.button(text="Download Receipts 🧾", callback_data="download_receipts")
+    builder.button(text="Download Results 📄", callback_data="download_results")
+    builder.button(text="Get Result Summary 🧾", callback_data="result_summary")
+    builder.button(text="Download Courses 📄", callback_data="download_courses")
+    builder.button(text="Download_Biodata 🧾", callback_data="download_biodata")
+    builder.button(text="Download Admission Forms 📄", callback_data="admission_forms")
     builder.adjust(2)
     return builder.as_markup()
 
